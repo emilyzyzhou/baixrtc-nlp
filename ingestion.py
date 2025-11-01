abee6e98c3e0218cea07f03d15ee2371048490f7
blairezh <113472440+blairezh@users.noreply.github.com>
2025-11-01 14:46:33 -0400

    pyttest import + test csv

--- ingestion.py
+++ ingestion.py
@@ -1,1 +1,6 @@
 import pandas as pd
+
+def load_csv():
+    df = pd.read_csv("sample.csv") #has some empty cells 
+    df.fillna("", inplace=True)
+    return df
--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,1 +1,2 @@
+import pytest
 #basic unit tests for reading in csv, validating cols
