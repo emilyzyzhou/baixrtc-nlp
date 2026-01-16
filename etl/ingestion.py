31031c176cf0411baea92724fc8e863ad4ab9a88
blairezh <113472440+blairezh@users.noreply.github.com>
2026-01-16 16:27:07 -0500

    moved df->csv to test method

--- etl/ingestion.py
+++ etl/ingestion.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+'''Extracts each sheet from source into df, returned as list of dfs'''
 def load_data(folder = "data"):
     dfs = []
     #sheets w long names will only match substring of length ~30
@@ -18,7 +19,7 @@ def load_data(folder = "data"):
             sheets_to_read = [sheet for sheet in data.sheet_names if sheet not in sheets_to_exclude]
             #create df for sheet + track which sheet it came from
             for sheet_name in sheets_to_read:
-                df = pd.read_excel(data, sheet_name=sheet_name).fillna("") #prevent na + type float64
+                df = pd.read_excel(data, sheet_name=sheet_name) #allow nan .fillna("") #prevent na + type float64
                 df["sheet_name"] = sheet_name
                 dfs.append(df)
             data.close()
@@ -29,16 +30,7 @@ def load_data(folder = "data"):
             df["sheet_name"] = os.path.splitext(file)[0] #use file name as sheet name
             dfs.append(df)
 
-    #compile all dfs into one
-    compiled_df = pd.concat(dfs, ignore_index=True)
-    #reorder so sheet_name is first 
-    cols = compiled_df.columns.tolist()
-    cols.remove("sheet_name")
-    compiled_df = compiled_df[["sheet_name"] + cols]
-    #output to csv for manual checking
-    compiled_df.to_csv('data/output.csv', index=False) #make sure you don't have csv open
-
-    return compiled_df
+    return dfs
 
 
 if __name__ == "__main__":
