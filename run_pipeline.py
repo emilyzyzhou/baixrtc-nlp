bbb4476990254fa368f55d912e3de86681ffdecf
summerrmu <summer.j.mu@gmail.com>
2026-04-22 19:43:31 -0400

    Revert "21 topic modeling"

--- run_pipeline.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from pipeline import run_pipeline
-
-if __name__ == "__main__":
-    responses_df, summary_df = run_pipeline()
-    print("Pipeline execution complete!")
